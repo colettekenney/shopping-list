e0f787bee9cd17f86d2aaf94f2ccd59973c3312f
colettekenney <colettemkenney@gmail.com>
2016-03-17 23:37:16 -0600

    change button css

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+// Clears all items on list 
+$(document).on('click', '#startover',function() {
+	$('.list-app').empty();
+	submissions = 0;
+})
+
+
+
+
+
+
+})
