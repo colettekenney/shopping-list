d8c8a4d0ceb968d9862c2edfd78eb9bd6ff3bf9b
colettekenney <colettemkenney@gmail.com>
2016-03-19 10:28:35 -0600

    add on.'hover' functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+//Adding items to the list:
+	$('form').submit(function(e) {
+		e.preventDefault();
+			//alert('pressed enter for adding new item');
+			$('ul.list-items').append('<li class="pending"><i class="fa fa-check"></i>'+$('input.new-item').val()+'</li>');
+			$('input.new-item').val('');
+	});
+
+//Adding 
+	$(document).on('hover', '.fa-check', function(e){
+		console.log(e.type);
+		if(e.type=='mouseenter'){
+		$(this).removeClass('fa-check').addClass('fa-minus');
+		}
+		else {
+		$(this).removeClass('fa-minus').addClass('fa-check');
+		}	
+	});
 
 
 
@@ -9,9 +27,10 @@ $(document).ready(function() {
 
 // Clears all items on list 
 $(document).on('click', '#startover',function() {
-	$('.list-app').empty();
-	submissions = 0;
-})
+	alert('clicked reset button');
+	//$('.list-app').empty();
+	//submissions = 0;
+});
 
 
 
