bc9d7c3e3cc4f3bcd5c5bd99be117d5e74e123dd
colettekenney <colettemkenney@gmail.com>
2016-03-19 13:15:31 -0600

    add erase all & erase checked

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
 
-//Adding items to the list:
+//Make list items sortable:
+	$( "ul.list-items" ).sortable({axis: "y"});
+    $( "ul.list-items" ).disableSelection();
+
+//Add items to the list:
 	$('form').submit(function(e) {
 		e.preventDefault();
 			//alert('pressed enter for adding new item');
-			$('ul.list-items').append('<li class="pending"><i class="fa fa-check"></i>'+$('input.new-item').val()+'</li>');
+			$('ul.list-items').append('<li class="pending"><i class="fa fa-square-o"></i>'+$('input.new-item').val()+'</li>');
 			$('input.new-item').val('');
 	});
 
-//Adding 
-	$(document).on('hover', '.fa-check', function(e){
-		console.log(e.type);
-		if(e.type=='mouseenter'){
-		$(this).removeClass('fa-check').addClass('fa-minus');
-		}
-		else {
-		$(this).removeClass('fa-minus').addClass('fa-check');
-		}	
+//Add toggling from pending to done:
+	$('.list-items').on('click', 'i.fa', function (){
+		$(this).toggleClass('fa-square-o fa-minus');
+		$(this).parent().toggleClass('pending done');
 	});
 
 
@@ -26,11 +25,19 @@ $(document).ready(function() {
 
 
 // Clears all items on list 
-$(document).on('click', '#startover',function() {
-	alert('clicked reset button');
-	//$('.list-app').empty();
-	//submissions = 0;
-});
+	$(document).on('click', '#clear-list',function() {
+		//alert('clicked reset button');
+		$('.list-container').empty();
+		submissions = 0;
+	});
+
+// Clears checked items only
+	$(document).on('click', '#clear-checked', function () {
+		$('.done').slideUp();
+	});
+
+
+
 
 
 
